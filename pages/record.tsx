7b430555436a7d6f88f841212d133e0a26c11d51
Maxim Bogovik <mbogovic@teamworkcommerce.com>
2023-06-11 16:33:43 +0300

    minor fix

--- pages/record.tsx
+++ pages/record.tsx
@@ -7,7 +7,8 @@ import { User, UserLesson } from '../interfaces';
 const RecordPage = () => {
   const router = useRouter();
   const { userId, lessonId, userName, lessonName} = router.query;
-  const user:UserLesson = {userId:userId[0], userName:userName[0], lessonId:lessonId[0], lessonName: lessonName[0]};
+  //const user:UserLesson = {userId:userId[0], userName:userName[0], lessonId:lessonId[0], lessonName: lessonName[0]};
+  const user:UserLesson = {userId:"1", userName:"1", lessonId:"1", lessonName: "1"};
   return (
   <Layout title="Запись урока">
     <UserRecordCard  user = {user}/>
